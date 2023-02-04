3774cdcff085fc6a2dde12b732b6adeb65fff4ba
Gador Cruz <gadorcruz@hotmail.com>
2023-02-04 17:29:21 +0000

    add validations and has many in models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :bike
   belongs_to :user
-
+  has_many :bikes
+  has_many :users
+  has_one_attached :photo
   validates :start_date, :end_date, presence: true
   validates :start_date, comparison: { greater_than: :end_date }
 end
