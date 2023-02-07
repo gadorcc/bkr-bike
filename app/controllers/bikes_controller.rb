97fb8a54db61992d2b961e92b34df4c2121eab5d
Yemi Arole <yarole7@gmail.com>
2023-02-07 21:24:11 +0000

    bikes controller v2

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,21 +1,40 @@
 class BikesController < ApplicationController
+  before_action :set_bike, only: [:show, :edit, :update, :destroy]
+
   def index
+    @bike = Bike.all
   end
 
   def show
   end
 
   def new
+    @bike = Bike.new
+  end
+
+  def create
+    @bike = Bike.new(bike_params)
+    @bike.save
+  end
+
+  def edit
   end
 
   def update
+    @bike.update(bike_params)
   end
 
   def destroy
+    @bike.destroy
   end
 
   private
 
-  def bikes_params
+  def bike_params
+    params.require(:bike).permit(:title, :description, :type, :price, :status)
+  end
+
+  def set_bike
+    @bike = Bike.find(params[:id])
   end
 end
