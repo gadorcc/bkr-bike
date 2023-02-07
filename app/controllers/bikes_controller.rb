8e1fe6f11555b5946db98bc6c16b8b8251419732
Yemi Arole <yarole7@gmail.com>
2023-02-07 20:59:26 +0000

    created empty controller and views

---
+++ app/controllers/bikes_controller.rb
@@ -0,0 +1,21 @@
+class BikesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def bikes_params
+  end
+end
