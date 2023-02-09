c16152089b84cc3dba9bc6fe652e3cd3f1959060
Gador Cruz <gadorcruz@hotmail.com>
2023-02-09 22:39:16 +0000

    add dotenv-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
+require "open-uri"
 puts "Cleaning up database..."
 Bike.destroy_all
 User.destroy_all
@@ -34,12 +35,14 @@ i = 0
 users.each do |user|
   i += 1
   puts "creating bike number #{i}"
-  Bike.create!(
+  file = URI.open("https://www.bikehighway.com/media/catalog/product/cache/8dfd20b727bccb444e9ac19dde97c5ca/0/0/0002_balance_bike_-_mint_green_angled_1024x1024.jpg")
+  bike = Bike.new(
     title: ["Amazing dirty bike", "Cool reliable biky picky","Bike me baby", "Colourful tiny bike"].sample,
     description: ["This bike is light, stiff, and comfortable. But you don’t need to scale step climbs to feel the Ultimate’s trademark performance. Experience pure freedom from the first turn of the crank.", "With fast wheels, comfortable tyres, pinpoint handling and long-distance geometry, this bike delivers control comfort and speed in equal measure. A great place to be for long rides, even on the roughest roads"].sample,
     bike_type: %w[electric road mountain folding kids hybrid city trike].sample,
     price: rand(5..45),
     user_id: user.id
   )
-  # bike.image.attach(io: Fileopen(Rails.root.join("/app/assets/images/sample.gif")), filename: "sample.gif")
+  bike.photo.attach(io: file, filename: "bike.png", content_type: "image/png")
+  bike.save!
 end
