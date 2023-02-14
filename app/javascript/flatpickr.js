d4bd23b996277a237430721d47aae825c52fb1aa
Simon Lance <simonlance200@hotmail.com>
2023-02-14 21:36:02 +0000

    working on flatpickr implementation

---
+++ app/javascript/flatpickr.js
@@ -0,0 +1,10 @@
+import flatpickr from "flatpickr";
+
+const initFlatpickr = () => {
+  flatpickr(".datepicker", {
+    enableTime: true,
+    minDate: "today",
+  })
+}
+
+export { initFlatpickr };
