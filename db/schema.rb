03c97cd6b4ea463119065dca9161247fbb9127c7
Gador Cruz <gadorcruz@hotmail.com>
2023-02-18 12:44:19 +0000

    fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_17_134356) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_14_203238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,12 +69,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_17_134356) do
   end
 
   create_table "reviews", force: :cascade do |t|
+    t.text "content"
     t.integer "rating"
     t.bigint "bike_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "content"
-    t.bigint "user_id"
     t.index ["bike_id"], name: "index_reviews_on_bike_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
