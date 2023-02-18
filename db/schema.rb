5d60495434c2995851aa37c9c313f15631855675
Iain Tidbury <iain@mainpages.online>
2023-02-18 14:04:59 +0000

    logo white navabar blue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_14_203238) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_18_122530) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,11 +71,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_203238) do
   create_table "reviews", force: :cascade do |t|
     t.text "content"
     t.integer "rating"
-    t.bigint "bike_id", null: false
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["bike_id"], name: "index_reviews_on_bike_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
@@ -99,6 +97,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_203238) do
   add_foreign_key "bikes", "users"
   add_foreign_key "bookings", "bikes"
   add_foreign_key "bookings", "users"
-  add_foreign_key "reviews", "bikes"
   add_foreign_key "reviews", "users"
 end
